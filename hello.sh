f72378991955c7fc900e29acee305f42dbead311
syam_devs <syamasundarraodatti@gmail.com>
2024-10-27 22:02:38 +0530

    new code

--- hello.sh
+++ hello.sh
@@ -3,3 +3,4 @@
 # Copyright 2019 The Kubernetes Authors.
 
 echo "hello shell script"
+TIMESTAMP=$(date +%F-%H-%M-%S)
--- mysql.sh
+++ mysql.sh
@@ -7,18 +7,17 @@ N="\e[0m"
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
-if [ $? -ne 0]
-then 
-    echo -e "error: $2 ... is $R FAILED $N"
-else    
-    echo "$2 .... is $G SUCCESS $N"
-fi
-
+    if [ $? -ne 0]
+    then 
+        echo -e "error: $2 ... is $R FAILED $N"
+    else    
+        echo "$2 .... is $G SUCCESS $N"
+    fi
 }
 
 if [ $ID -ne 0 ]
 then    
-    echo -e "$R error: please run this script with root access"
+    echo -e "$R error: please run this script with root access $N"
 else
     echo "you are $G root $N"
 fi
