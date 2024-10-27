dd33c39628c5799c2b1a3e921f6c56f6aba097b4
syam_devs <syamasundarraodatti@gmail.com>
2024-10-27 18:00:48 +0530

    added 2nd script

---
+++ variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON1 = "John"
+PERSON2 = "Doe"
+
+echo "My name is $PERSON1 $PERSON2"
+echo "My name is ${PERSON1} ${PERSON2}"
+echo "My name is $PERSON1 $PERSON2"
+echo "My name is ${PERSON1} ${PERSON2}"
+echo "My name is $PERSON1 $PERSON2"
+echo "My name is ${PERSON1} ${PERSON2}"
+echo "My name is $PERSON1 $PERSON2"
+echo "My name is ${PERSON1} ${PERSON2}"
+
