a8a439f21752f9b94fec509e630c1e5eba5d7bb5
syam_devs <syamasundarraodatti@gmail.com>
2024-10-28 22:34:21 +0530

    check code

--- mango.sh
+++ mango.sh
@@ -9,16 +9,16 @@ N="\e[0m"
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
+LOGFILE="/tmp/$(basename $0)-$TIMESTAMP.log"
 
-echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+echo "script started executing at $TIMESTAMP" | tee -a $LOGFILE
 
 VALIDATE() {
     if [ $1 -ne 0 ]; then
-        echo -e "error: $2 ... is $R FAILED $N" &>> $LOGFILE
+        echo -e "error: $2 ... is ${R}FAILED${N}" | tee -a $LOGFILE
         exit 1
     else
-        echo -e "$2 .... is $G SUCCESS $N" &>> $LOGFILE
+        echo -e "$2 .... is ${G}SUCCESS${N}" | tee -a $LOGFILE
     fi
 }
 
@@ -30,27 +30,21 @@ else
 fi
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
-
 VALIDATE $? "copying mongo.repo file"
 
 yum install mongodb-org -y &>> $LOGFILE
-
 VALIDATE $? "installing mongodb-org"
 
 systemctl enable mongod &>> $LOGFILE
-
 VALIDATE $? "enabling mongod service"
 
 systemctl start mongod &>> $LOGFILE
-
 VALIDATE $? "starting mongod service"
 
-echo "mongodb installation completed"
+echo "mongodb installation completed" | tee -a $LOGFILE
 
 # Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $LOGFILE
 
 systemctl restart mongod &>> $LOGFILE
-
 VALIDATE $? "restarting mongod service"
